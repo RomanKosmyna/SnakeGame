5945c6bf0291d4c3f7fa8a25a667ce00aeb357c3
RomanKosmyna <roma.kosmyna@gmail.com>
2024-01-03 20:48:45 +0200

    Create ISnake interface. Implemented this interface in Snake class.

---
+++ SnakeGame/ISnake.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+interface ISnake
+{
+    List<Point> SnakeBody { get; }
+    List<Point> BodyPart { get; }
+    Point Head {  get; }
+
+    void Instantiate();
+}
---
+++ SnakeGame/Snake.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+public class Snake : ISnake
+{
+    private List<Point> snakeBody = [];
+    private List<Point> bodyPart = [];
+    private Point head = new();
+
+    public List<Point> SnakeBody { get { return snakeBody; } }
+    public List<Point> BodyPart { get { return bodyPart; } }
+    public Point Head { get { return head; } }
+
+    public void Instantiate()
+    {
+
+    }
+}
