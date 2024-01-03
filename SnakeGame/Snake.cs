da66dd0584bb39ab60ce84d9de2a9360ae23e1d7
RomanKosmyna <roma.kosmyna@gmail.com>
2024-01-04 00:07:44 +0200

    Added functionality to move a snake. Also added methods to render it.

--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -1,17 +1,139 @@
 ﻿using System.Drawing;
+using System.Xml.Linq;
 
 public class Snake : ISnake
 {
-    private List<Point> snakeBody = [];
-    private List<Point> bodyPart = [];
-    private Point head = new();
+    private List<Point> _snakeBody = [];
+    private readonly List<Point> _bodyPart = [];
+    private Point _head;
 
-    public List<Point> SnakeBody { get { return snakeBody; } }
-    public List<Point> BodyPart { get { return bodyPart; } }
-    public Point Head { get { return head; } }
+    private readonly int _initialSize = 1;
+
+    private readonly int _leftIndent;
+    private readonly int _topIndent;
+
+    public List<Point> SnakeBody { get { return _snakeBody; } }
+    public List<Point> BodyPart { get { return _bodyPart; } }
+    public Point Head { get { return _head; } }
+
+    public int LeftIndent { get { return _leftIndent; } }
+    public int TopIndent { get { return _topIndent; } }
+
+    public Snake(int leftIndent, int topIndent)
+    {
+        _leftIndent = leftIndent;
+        _topIndent = topIndent;
+    }
 
     public void Instantiate()
     {
+        for (int i = 0; i < _initialSize; i++)
+        {
+            Point bodySegment = new Point(LeftIndent + i, TopIndent);
+            _snakeBody.Add(bodySegment);
+        }
+    }
 
+    public void MoveLeft()
+    {
+        List<Point> newSnake = [];
+
+        foreach (Point bodySegment in _snakeBody)
+        {
+            int newX = bodySegment.X - 1;
+            newSnake.Add(new Point(newX, bodySegment.Y));
+        }
+
+        if (newSnake.Count > _initialSize)
+        {
+            _snakeBody.RemoveAt(_snakeBody.Count - 1);
+        }
+
+        _snakeBody = newSnake;
+
+        UpdateSnakeDisplay();
+    }
+
+    public void MoveRight()
+    {
+        List<Point> newSnake = [];
+
+        foreach (Point bodySegment in _snakeBody)
+        {
+            int newX = bodySegment.X + 1;
+            newSnake.Add(new Point(newX, bodySegment.Y));
+        }
+
+        if (newSnake.Count > _initialSize)
+        {
+            _snakeBody.RemoveAt(0);
+        }
+
+        _snakeBody = newSnake;
+
+        UpdateSnakeDisplay();
+    }
+
+    public void MoveTop()
+    {
+        List<Point> newSnake = [];
+
+        foreach (Point bodySegment in _snakeBody)
+        {
+            int newY = bodySegment.Y - 1;
+            newSnake.Add(new Point(bodySegment.X, newY));
+        }
+
+        if (newSnake.Count > _initialSize)
+        {
+            _snakeBody.RemoveAt(0);
+        }
+
+        _snakeBody = newSnake;
+
+        UpdateSnakeDisplay();
+    }
+
+    public void MoveBottom()
+    {
+        List<Point> newSnake = [];
+
+        foreach (Point bodySegment in _snakeBody)
+        {
+            int newY = bodySegment.Y + 1;
+            newSnake.Add(new Point(bodySegment.X, newY));
+        }
+
+        if (newSnake.Count > _initialSize)
+        {
+            _snakeBody.RemoveAt(0);
+        }
+
+        _snakeBody = newSnake;
+
+        UpdateSnakeDisplay();
+    }
+
+    public void UpdateSnakeDisplay()
+    {
+        Console.Clear();
+
+        foreach (Point part in _snakeBody)
+        {
+            Console.SetCursorPosition(part.X + LeftIndent, part.Y + TopIndent);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write(" ");
+            Console.ResetColor();
+        }
+    }
+
+    public void SnakeCount()
+    {
+        Console.WriteLine(_snakeBody.Count);
+        
+        foreach (Point part in _snakeBody)
+        {
+            Console.WriteLine($"{part.X} {part.Y}");
+        }
     }
 }
--- SnakeGame/SnakeGame.cs
+++ SnakeGame/SnakeGame.cs
@@ -2,6 +2,86 @@
 {
     public void Run()
     {
+        Console.CursorVisible = false;
+
+        Snake snake = new(5, 11);
+
+        snake.Instantiate();
+        snake.UpdateSnakeDisplay();
+
+        while (true)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+
+                ConsoleKey currentDirectionLeft = ConsoleKey.A;
+
+                switch (key)
+                {
+                    case ConsoleKey.A:
+                        {
+                            ConsoleKey newKey = key;
+                            do
+                            {
+                                snake.MoveLeft();
+                                if (Console.KeyAvailable)
+                                {
+                                    newKey = Console.ReadKey(true).Key;
+                                }
+                            }
+                            while (newKey != ConsoleKey.W && newKey != ConsoleKey.D && newKey != ConsoleKey.S);
+                            Thread.Sleep(200);
+                            break;
+                        }
+                    case ConsoleKey.D:
+                        {
+                            ConsoleKey newKey = key;
+                            do
+                            {
+                                snake.MoveRight();
+                                if (Console.KeyAvailable)
+                                {
+                                    newKey = Console.ReadKey(true).Key;
+                                }
+                            }
+                            while (newKey != ConsoleKey.A && newKey != ConsoleKey.W && newKey != ConsoleKey.S);
+                            Thread.Sleep(200);
+                            break;
+                        }
+                    case ConsoleKey.W:
+                        {
+                            ConsoleKey newKey = key;
+                            do
+                            {
+                                snake.MoveTop();
+                                if (Console.KeyAvailable)
+                                {
+                                    newKey = Console.ReadKey(true).Key;
+                                }
+                            }
+                            while (newKey != ConsoleKey.A && newKey != ConsoleKey.D && newKey != ConsoleKey.S);
+                            Thread.Sleep(200);
+                            break;
+                        }
+                    case ConsoleKey.S:
+                        {
+                            ConsoleKey newKey = key;
+                            do
+                            {
+                                snake.MoveBottom();
+                                if (Console.KeyAvailable)
+                                {
+                                    newKey = Console.ReadKey(true).Key;
+                                }
+                            }
+                            while (newKey != ConsoleKey.A && newKey != ConsoleKey.W && newKey != ConsoleKey.D);
+                            Thread.Sleep(200);
+                            break;
+                        }
+                }
+            }
+        }
 
     }
 }
