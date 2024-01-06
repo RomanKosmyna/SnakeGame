32942967c7d8b4d165a26d85b42d40f1d1b4c289
RomanKosmyna <roma.kosmyna@gmail.com>
2024-01-06 06:32:30 +0200

    Moved movement methods into a separate class - SnakeMovement.

--- SnakeGame/SnakeGame.cs
+++ SnakeGame/SnakeGame.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using static System.Console;
+using System.Drawing;
 
 public class SnakeGame
 {
@@ -11,15 +12,18 @@ public class SnakeGame
 
     private bool gameStatus = true;
 
+    private readonly SnakeMovement snakeMovement;
+
     public SnakeGame()
     {
         menu = new();
         snake = new(startingPoint, startingSize);
+        snakeMovement = new(snake);
     }
 
     public void Run()
     {
-        Console.CursorVisible = false;
+        CursorVisible = false;
 
         LoadGameObjects();
 
@@ -43,20 +47,20 @@ public class SnakeGame
                 menu.DrawScore();
             }
 
-            if (Console.KeyAvailable)
+            if (KeyAvailable)
             {
-                SetPreviousKey(ref previousKey);
+                snakeMovement.SetPreviousKey(ref previousKey);
             }
 
-            SnakeMovement(previousKey);
+            snakeMovement.SnakeControl(previousKey);
 
             gameStatus = snake.IsSnakeCollisionNotDetected();
         }
 
         GameStatus.DrawGameStatus(false);
 
-        Console.ReadKey();
-        Console.SetCursorPosition(6, 22);
+        ReadKey();
+        SetCursorPosition(6, 22);
     }
 
     protected void LoadGameObjects()
@@ -66,58 +70,5 @@ public class SnakeGame
         snake.DrawSnake();
 
         menu.LaunchMenu();
-    }
-
-    protected void SnakeMovement(ConsoleKey previousKey)
-    {
-        switch (previousKey)
-        {
-            case ConsoleKey.W:
-            case ConsoleKey.UpArrow:
-                {
-                    snake.Move(Snake.Direction.Up);
-                    break;
-                }
-            case ConsoleKey.S:
-            case ConsoleKey.DownArrow:
-                {
-                    snake.Move(Snake.Direction.Down);
-                    break;
-                }
-            case ConsoleKey.A:
-            case ConsoleKey.LeftArrow:
-                {
-                    snake.Move(Snake.Direction.Left);
-                    break;
-                }
-            case ConsoleKey.D:
-            case ConsoleKey.RightArrow:
-                {
-                    snake.Move(Snake.Direction.Right);
-                    break;
-                }
-        }
-    }
-
-    protected static void SetPreviousKey(ref ConsoleKey previousKey)
-    {
-        var key = Console.ReadKey(true).Key;
-
-        if ((key == ConsoleKey.W || key == ConsoleKey.UpArrow) && previousKey != ConsoleKey.S)
-        {
-            previousKey = ConsoleKey.W;
-        }
-        else if ((key == ConsoleKey.S || key == ConsoleKey.DownArrow) && previousKey != ConsoleKey.W)
-        {
-            previousKey = ConsoleKey.S;
-        }
-        else if ((key == ConsoleKey.A || key == ConsoleKey.LeftArrow) && previousKey != ConsoleKey.D)
-        {
-            previousKey = ConsoleKey.A;
-        }
-        else if ((key == ConsoleKey.D || key == ConsoleKey.RightArrow) && previousKey != ConsoleKey.A)
-        {
-            previousKey = ConsoleKey.D;
-        }
     }
 }
---
+++ SnakeGame/SnakeMovement.cs
@@ -0,0 +1,64 @@
+﻿using static System.Console;
+
+public class SnakeMovement
+{
+    private readonly Snake _snake;
+
+    public SnakeMovement(Snake snake)
+    {
+        _snake = snake;
+    }
+
+    public void SnakeControl(ConsoleKey previousKey)
+    {
+        switch (previousKey)
+        {
+            case ConsoleKey.W:
+            case ConsoleKey.UpArrow:
+                {
+                    _snake.Move(Snake.Direction.Up);
+                    break;
+                }
+            case ConsoleKey.S:
+            case ConsoleKey.DownArrow:
+                {
+                    _snake.Move(Snake.Direction.Down);
+                    break;
+                }
+            case ConsoleKey.A:
+            case ConsoleKey.LeftArrow:
+                {
+                    _snake.Move(Snake.Direction.Left);
+                    break;
+                }
+            case ConsoleKey.D:
+            case ConsoleKey.RightArrow:
+                {
+                    _snake.Move(Snake.Direction.Right);
+                    break;
+                }
+        }
+    }
+
+    public void SetPreviousKey(ref ConsoleKey previousKey)
+    {
+        var key = ReadKey(true).Key;
+
+        if ((key == ConsoleKey.W || key == ConsoleKey.UpArrow) && previousKey != ConsoleKey.S)
+        {
+            previousKey = ConsoleKey.W;
+        }
+        else if ((key == ConsoleKey.S || key == ConsoleKey.DownArrow) && previousKey != ConsoleKey.W)
+        {
+            previousKey = ConsoleKey.S;
+        }
+        else if ((key == ConsoleKey.A || key == ConsoleKey.LeftArrow) && previousKey != ConsoleKey.D)
+        {
+            previousKey = ConsoleKey.A;
+        }
+        else if ((key == ConsoleKey.D || key == ConsoleKey.RightArrow) && previousKey != ConsoleKey.A)
+        {
+            previousKey = ConsoleKey.D;
+        }
+    }
+}
